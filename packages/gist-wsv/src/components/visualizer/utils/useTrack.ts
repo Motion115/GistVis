65f56e963fe22bd8e92de8e8bebb01aec8e8a0a5
vakesamahere <vakegamer@163.com>
2025-03-09 14:45:10 +0800

    fix: update package dependencies and remove unused imports in useTrack

--- packages/gist-wsv/src/components/visualizer/utils/useTrack.ts
+++ packages/gist-wsv/src/components/visualizer/utils/useTrack.ts
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from 'react';
-import { useAtom } from 'jotai';
 // import { articleIdAtom } from '../../../globalState';
 const generateUniqueIdentifier = () => Math.random().toString(36).substr(2, 9);
 const useTrackVisit = (id: string) => {
