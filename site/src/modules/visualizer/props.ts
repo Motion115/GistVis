c3adab6aeaa8264bf01ec50450cd8e25e5e49d5b
vakesamahere <vakegamer@163.com>
2025-03-16 03:52:02 +0800

    delete visualizer modules in site

--- site/src/modules/visualizer/props.ts
+++
@@ -1,8 +0,0 @@
-import { DataSpec } from './types';
-
-export interface HorizontalStackedBarProps {
-  dataSpec: DataSpec[];
-  colorScale: d3.ScaleOrdinal<string, string, never>;
-  selectedEntity: string;
-  setSelectedEntity: (entity: string) => void;
-}
