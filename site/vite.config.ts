6c481177c0de17a6b59a3a9add92074daf1a2a9e
vakesamahere <vakegamer@163.com>
2025-03-23 16:08:17 +0800

    Chore: Add path aliases for improved module resolution

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,4 +9,9 @@ export default defineConfig({
     hmr: true,
   },
   base: '/GistVis/',
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+    },
+  },
 });
