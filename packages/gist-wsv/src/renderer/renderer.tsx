4ae4e6e79a3a527414fbabf379379830ae0f1afd
Lydia-1204 <2351591@tongji.edu.cn>
2025-04-25 13:29:19 +0800

    Feat(visualizer): add checkDataspecValidity (#87)
    * change checkDataspecValidity
    
    * Refactor Switch-Case to Key-Value Pairs and Encapsulate Code in Class Methods

--- packages/gist-wsv/src/renderer/renderer.tsx
+++ packages/gist-wsv/src/renderer/renderer.tsx
@@ -1,4 +1,4 @@
-import { GistvisSpec, InsightType, paragraphSpec } from '../components/types';
+import { DataSpec, GistvisSpec, InsightType, paragraphSpec } from '../components/types';
 import {
   ComparisonTextRenderer,
   ExtremeTextRenderer,
@@ -12,6 +12,68 @@ import { recommendValidTypes } from '../utils/utils';
 import FallBackCase from '../components/widgets/fallbackVis';
 import React from 'react';
 
+class DataSpecValidator {
+  private static checkTrendValidity(dataSpec: DataSpec[]): boolean {
+    return true;
+  }
+
+  private static checkComparisonValidity(dataSpec: DataSpec[]): boolean {
+    return dataSpec.length >= 2 && dataSpec.every(data => !isNaN(data.value));
+  }
+
+  private static checkProportionValidity(dataSpec: DataSpec[]): boolean {
+    const values = dataSpec.map(data => data.value).filter(val => !isNaN(val));
+    if (values.length === 0) 
+      return false;
+
+    const isZeroToOne = values.every(val => val >= 0 && val <= 1);
+    const isZeroToHundred = values.every(val => val >= 0 && val <= 100);
+    return isZeroToOne || isZeroToHundred;
+  }
+
+  private static checkExtremeOrValueValidity(dataSpec: DataSpec[]): boolean {
+    return dataSpec.every(data => 
+      data.space &&
+      data.feature &&
+      !isNaN(data.value)
+    );
+  }
+
+  private static checkRankValidity(dataSpec: DataSpec[]): boolean {
+    return dataSpec.length >= 2 && 
+           dataSpec.every(data => 
+             data.space &&
+             data.feature &&
+             !isNaN(data.value)
+           );
+  }
+
+  private static validatorMap: Record<InsightType, (dataSpec: DataSpec[]) => boolean> = {
+    trend: DataSpecValidator.checkTrendValidity,
+    comparison: DataSpecValidator.checkComparisonValidity,
+    proportion: DataSpecValidator.checkProportionValidity,
+    extreme: DataSpecValidator.checkExtremeOrValueValidity,
+    value: DataSpecValidator.checkExtremeOrValueValidity,
+    rank: DataSpecValidator.checkRankValidity,
+    noType: () => true
+  };
+
+  public static checkValidity(item: GistvisSpec): boolean {
+    const { insightType } = item.unitSegmentSpec;
+    
+    if (!item.dataSpec) {
+      return insightType === 'trend';
+    }
+    
+    if (item.dataSpec.length === 0) {
+      return insightType === 'trend';
+    }
+
+    const validator = this.validatorMap[insightType];
+    return validator ? validator(item.dataSpec) : true;
+  }
+}
+
 export const GistvisVisualizer: React.FC<{ datafactSpec: paragraphSpec[] }> = ({ datafactSpec }) => {
   const renderMap = {
     noType: (item: GistvisSpec) => <PlainTextRenderer gistvisSpec={item} />,
@@ -23,23 +85,10 @@ export const GistvisVisualizer: React.FC<{ datafactSpec: paragraphSpec[] }> = ({
     value: (item: GistvisSpec) => <ValueTextRenderer gistvisSpec={item} />,
     fallback: (item: GistvisSpec) => <FallBackCase gistvisSpec={item} />,
   };
-  // console.log(JSON.stringify(datafactSpec, null, 2))
+   // console.log(JSON.stringify(datafactSpec, null, 2))
 
-  const checkDataspecValidity = (item: GistvisSpec) => {
-    const dataSpec = item.dataSpec ?? [];
-    // if (dataSpec.length === 0) {
-    //   return false;
-    // }
-    // else {
-    //   let spaceList = lodash.uniq(dataSpec.map((data) => data.space));
-    //   let featureList = lodash.uniq(dataSpec.map((data) => data.feature));
-    //   if (spaceList.length > 1 || featureList.length > 1) {
-    //     return false;
-    //   }
-    //   else {
-    return true;
-    //   }
-    // }
+  const checkDataspecValidity = (item: GistvisSpec): boolean => {
+    return DataSpecValidator.checkValidity(item);
   };
 
   return (
